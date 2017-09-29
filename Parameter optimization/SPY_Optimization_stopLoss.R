f8bef2f7848896caee622658fbdc8e6ad5c4dc2a
Boniface316 <boniface.yogi@gmail.com>
2017-09-29 19:12:23 -0400

    StopLoss Optimization

--- Parameter optimization/SPY_Optimization_stopLoss.R
+++ Parameter optimization/SPY_Optimization_stopLoss.R
@@ -1,22 +1,20 @@
 rm.strat(qs.strategy) #clear the strategy before the re-run
-#rm(list = ls()) #Clear all data
+rm(list = ls()) #Clear all data
 
 #Setup library----------------------------------------------------------------
 library("quantstrat")
-library("rgl")
 
 # read in data -----------------------------------------------------------------
-SPY <-get(getSymbols("SPY"))["2011"]
+SPY <-get(getSymbols("SPY"))
 
 #Set the parameters
 initDate <- "2010-12-31" #Date of initiation
 
-nSlow <- 30 #Slow moving average sample size
-nFast <- 10 #Fast moving average sample size
+nSlow <- 75 #Slow moving average sample size
+nFast <- 5 #Fast moving average sample size
 syms <- "SPY" #The stock that we are interested in
 initEq <- 1000 #Initial equity
-FastSMA <- 1:10 #Range of moving average
-SlowSMA <- seq(20,80,5) #Range of moving average
+stopLossRange <- seq(0.05, 10, length.out = 50)/100
 threshold <- 0.0005 #Threshold
 orderqty <- 100 #Qty order
 
@@ -29,6 +27,7 @@ initOrders(portfolio = qs.strategy, initDate = initDate) #Initiate account
 
 strategy(qs.strategy, store = TRUE) #Store all the events in the strategy
 
+#----------Add indicators-----------------------
 add.indicator(qs.strategy,
               name = "SMA",
               arguments = list (
@@ -47,7 +46,7 @@ add.indicator(qs.strategy,
               label = "nSlow"
 )
 
-#--------------------------------------------------------------------
+#----------Add signals-----------------------
 add.signal(qs.strategy,
            name = "sigCrossover",
            arguments = list(
@@ -65,6 +64,8 @@ add.signal(qs.strategy,
            ),
            label = "short")
 
+#----------Add rule-----------------------
+#EnterLONG
 add.rule(qs.strategy,
          name = "ruleSignal",
          arguments = list(sigcol = "long",
@@ -74,12 +75,12 @@ add.rule(qs.strategy,
                           prefer = "High",
                           threshold = threshold,
                           orderqty = +orderqty,
-                          replace = FALSE
-                          ),
+                          replace = FALSE,
+                          orderset = "ocolong"),
          type = "enter",
          label = "EnterLONG")
 
-
+#Exit2SHORT
 add.rule(qs.strategy,
          name = "ruleSignal",
          arguments = list (sigcol = "short",
@@ -87,44 +88,56 @@ add.rule(qs.strategy,
                            orderside = "long",
                            ordertype = "market",
                            orderqty = "all",
-                           replace = TRUE
-                           ),
+                           replace = TRUE,
+                           orderset = "ocolong"),
          type = "exit",
-         label = "Exit2SHORT")
+         label = "Exit2SHORT"
+)
+
+#Stop Loss
+add.rule(qs.strategy,
+         name = "ruleSignal",
+         arguments = list (sigcol = "long",
+                           sigval = TRUE,
+                           replace = FALSE,
+                           orderside = "long",
+                           ordertype = "stoplimit",
+                           tmult = TRUE,
+                           treshold = quote(stopLoss),
+                           orderqty = "all",
+                           orderset = "ocolong"),
+         type = "chain",
+         parent = "EnterLONG",
+         label = "StopLossLONG",
+         enabled = TRUE)
 
 add.distribution(qs.strategy,
-                 paramset.label = "SMA",
-                 component.type = "indicator",
-                 component.label = "nFast",
-                 variable = list(n = FastSMA),
-                 label = "nFAST")
-
-add.distribution(qs.strategy,
-                 paramset.label = "SMA",
-                 component.type = "indicator",
-                 component.label = "nSlow",
-                 variable = list(n = SlowSMA),
-                 label = "nSLOW")
-
-add.distribution.constraint(qs.strategy,
-                            paramset.label = "SMA",
-                            distribution.label.1 = "nFAST",
-                            distribution.label.2 = "nSLOW",
-                            operator = "<",
-                            label = "SMA")
+                 paramset.label = "stopLossLONG",
+                 component.type = "chain",
+                 component.label = "StopLossLONG",
+                 variable = list(threshold = stopLossRange),
+                 label = "StopLossLONG")
 
 
-out <- apply.paramset(qs.strategy, paramset.label = "SMA", portfolio.st = qs.strategy, account.st = qs.strategy, nsamples = 0)
+#----------Apply strategy-----------------------
+out <- apply.paramset(qs.strategy,
+                      paramset.label = "stopLossLONG",
+                      portfolio.st = qs.strategy,
+                      account.st = qs.strategy,
+                      nsamples = 0,
+                      verbose = TRUE)
 
 updatePortf(qs.strategy)
 updateAcct(qs.strategy)
 updateEndEq(qs.strategy)
 
-tS <- out$tradeStats
-idx <- order(tS[,1],tS[,2])
-tS <- tS[idx,]
+tradeStats <- out$tradeStats
+graphData <- cbind(tradeStats$StopLossLONG, tradeStats$Net.Trading.PL)
+colnames(graphData) <- c("Stop Loss %", "Net PL")
+graphData[,1] <- graphData[,1]*100
+plot(graphData, main = "SPY - PL Vs stop loss %")
+View(graphData)
+View(tradeStats)
 
-tradeGraphs(stats = tS, free.params = c("nFAST", "nSLOW"),
-            statistics = c("Net.Trading.PL", ""))
 
 
