ef68f28945a8f0537bfeae77364043d5a624a386
boniface316 <boniface.yogi@gmail.com>
2018-03-18 23:03:54 -0400

    changes

--- Demo/LUXOR_SPY.R
+++ Demo/LUXOR_SPY.R
@@ -1,82 +1,106 @@
 #Setup library----------------------------------------------------------------
 library("quantstrat")
 
+#Set the currency and stock
 currency("USD")
 stock("SPY",currency="USD",multiplier=1)
 
+initDate <- "2006-01-02" #Date of initiation
+from <- "2007-01-03" #Start date of the data set
+to <- "2018-03-18" #End date of the data set
+initEq <- 1000 #Initial equity
+nFast <- 10
+nSlow <- 30
+orderqty <- 100
 # read in data -----------------------------------------------------------------
-getSymbols("SPY")
+getSymbols("SPY", from = from, to = to)
 
 #Set the parameters
-initDate <- "2012-12-31" #Date of initiation
-from <- "2013-01-01" #Start date of the data set
-to <- "2013-01-30" #End date of the data set
-initEq <- 1000 #Initial equity
 
-mkData <- Cl(sym) #Store only the close price of the data
-colnames(mkData) <- "Close" #Naming the column
 
 #-------------------------Initiate portfolio and account-------------------------------------------
-qs.strategy <- "qsFaber" #Name the strategy
+strategy.st <- "luxor" #Name the strategy
+account.st <- "luxor"
+portfolio.st <- "luxor"
 
-initPortf(qs.strategy, "mkData", initDate = initDate) #Initiate portfolio
-initAcct(qs.strategy, portfolios = qs.strategy, initDate = initDate, initEq = initEq) #Initiate account
-initOrders(portfolio = qs.strategy, initDate = initDate) #Initiate account
 
-strategy(qs.strategy, store = TRUE) #Store all the events in the strategy
+initPortf(strategy.st,symbols = "SPY", initDate = initDate) #Initiate portfolio
+initAcct(account.st, portfolios = portfolio.st, initDate = initDate, initEq = initEq) #Initiate account
+initOrders(portfolio = portfolio.st, initDate = initDate) #Initiate account
 
-#-------------------------Add indicator-------------------
-#Calculate the moving average for 30 periods
-add.indicator(strategy = qs.strategy,
-              name = "SMA",
-              arguments = list (x = mkData$Close,
-                                n = 30),
-              label = "SMA30"
-              )
+strategy(strategy.st, store = TRUE) #Store all the events in the strategy
 
-#-------------------------Add signals ---------------------
-add.signal(qs.strategy,
-           name = "sigCrossover",
-           arguments = list(columns = c("Close", "SMA30"),
-                            relationship = "gt"),
-           label = "Cl.gt.SMA")
+### indicators
 
-add.signal(qs.strategy,
-           name = "sigCrossover",
-           arguments = list(columns = c("Close", "SMA30"),
-                            relationship = "lt"),
-           label = "Cl.lt.SMA")
-#-------------------------Add rules to buy and sell ---------
-#Buy 100 when the enter signal is triggered and sell all when the exit signal is triggered
-add.rule(qs.strategy, name = "ruleSignal",
-         arguments = list(sigcol = "Cl.gt.SMA",
-                          sigval = TRUE,
-                          orderqty = 100,
-                          ordertype = "market",
-                          orderside = "long"),
-         type = "enter")
 
-add.rule(qs.strategy, name = "ruleSignal",
-         arguments = list (sigcol = "Cl.lt.SMA",
-                           sigval = TRUE,
-                           orderqty = "all",
-                           ordertype = "market",
-                           orderside = "long"),
-         type = "exit")
-#-------------------------Apply Strategy ------------
-applyStrategy(strategy = qs.strategy, portfolios = qs.strategy)
-getTxns(Portfolio = qs.strategy, Symbol = "mkData")
+add.indicator(strategy.st, name = "SMA",
+              arguments = list(
+                x = quote(Cl(mktdata)[,1]),
+                n = nFast
+              ),
+              label="nFast"
+)
 
-#-------------------------Update portfolio, account and equity------------
-updatePortf(qs.strategy)
-updateAcct(qs.strategy)
-updateEndEq(qs.strategy)
+add.indicator(strategy.st, name="SMA",
+              arguments = list(
+                x = quote(Cl(mktdata)[,1]),
+                n = nSlow
+              ),
+              label="nSlow"
+)
 
-#-------------------------Plot performance-------------------
-myTheme <- chart_theme()
-myTheme$col$dn.col<-'lightblue'
-myTheme$col$dn.border <- 'lightgray'
-myTheme$col$up.border <- 'lightgray'
+### signals
 
-chart.Posn(qs.strategy, Symbol = 'mkData', Dates = '2013::',theme=myTheme,
-           TA='add_SMA(n=10,col=4, on=1, lwd=2)')
+add.signal(strategy.st, name='sigCrossover',
+           arguments = list(
+             columns=c("nFast","nSlow"),
+             relationship="gte"
+           ),
+           label='long'
+)
+
+add.signal(strategy.st, name='sigCrossover',
+           arguments = list(
+             columns=c("nFast","nSlow"),
+             relationship="lt"
+           ),
+           label='short'
+)
+
+### rules
+
+
+add.rule(strategy.st, name='ruleSignal',
+         arguments=list(sigcol='short', sigval=TRUE,
+                        orderside='long' ,
+                        ordertype='market',
+                        orderqty='all',
+                        replace=TRUE
+         ),
+         type='exit',
+         label='Exit2SHORT'
+)
+
+add.rule(strategy.st, name='ruleSignal',
+         arguments=list(sigcol='long' , sigval=TRUE,
+                        orderside='long' ,
+                        ordertype='market', 
+                        orderqty=orderqty,
+                        replace=FALSE
+         ),
+         type='enter',
+         label='EnterLONG'
+)
+
+
+###############################################################################
+
+applyStrategy(strategy.st, portfolio.st)
+
+#update
+updatePortf(strategy.st)
+updateAcct(strategy.st)
+updateEndEq(strategy.st)
+
+#Plot chart
+chart.Posn(portfolio.st, "SPY")
